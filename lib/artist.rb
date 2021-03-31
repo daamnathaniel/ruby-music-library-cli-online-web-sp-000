6eef334723ca78e8b6921978f1e6aff57cc05587
Nathaniel Adam <daamnathaniel@gmail.com>
2021-03-31 17:08:02 -0500

    commit tree 4537feba068496fd8ab7a1547459186018cd5de7

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+	attr_accessor :name
+	@@all = []
+
+	def initialize(name)
+		@name = name
+		@songs = []
+
+	end
+
+	def self.all
+		@@all
+	end
+
+	def save
+		@@all << self
+	end
+
+	def self.destroy_all
+		@@all = []
+	end
+
+	def self.create(name)
+		new(name).tap{|s|s.save}
+	end
+
+end
